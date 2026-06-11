457403f285c6b5217101a2b710f768ef2b16231e
João Pedro <129764561+joaopedrocmr@users.noreply.github.com>
2026-06-11 17:40:22 +0000

    add  and  files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-function status(req, res) {
-  res.status(200).json({chave: " Um futuro senior está programando essa página!!"})
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  res
+    .status(200)
+    .json({ chave: " Um futuro senior está programando essa página!!" });
 }
 
-export default status
+export default status;
