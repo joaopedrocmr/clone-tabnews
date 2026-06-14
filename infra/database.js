b29c03d30d3b185c4a0dd842a2cd7930a7916d9e
João Pedro <129764561+joaopedrocmr@users.noreply.github.com>
2026-06-14 21:42:13 +0000

    makes `database.js` `client.connect` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -14,13 +14,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    
+    console.log(error);
+    throw error;
   } finally {
     await client.end();
   }
-  
-  
-
 }
 
 export default {
