6d664703ebf8a2519b7a7b6c96a44a3578648dca
João Pedro <129764561+joaopedrocmr@users.noreply.github.com>
2026-06-14 23:54:21 +0000

    handles database ssl connection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,34 +1,32 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
+  const updatedAt = new Date().toISOString();
 
-  const updatedAt = new Date().toISOString()
-  
-   const databaseVersionresult = await database.query("SHOW server_version")
-   const databaseVersion = databaseVersionresult.rows[0].server_version
+  const databaseVersionresult = await database.query("SHOW server_version");
 
-  const maxConnectionsresult = await database.query("SHOW max_connections")
-  const maxConnections = maxConnectionsresult.rows[0].max_connections
+  const databaseVersion = databaseVersionresult.rows[0].server_version;
 
-  const databaseName = process.env.POSTGRES_DB
+  const maxConnectionsresult = await database.query("SHOW max_connections");
+  const maxConnections = maxConnectionsresult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
   const openConnectionsresult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
-    values: [databaseName]
-  }  
-  )
-  const openConnections = openConnectionsresult.rows[0].count
- 
+    values: [databaseName],
+  });
+  const openConnections = openConnectionsresult.rows[0].count;
+
   res.status(200).json({
     updated_at: updatedAt,
-    dependencies:{
-      database:{
+    dependencies: {
+      database: {
         version: databaseVersion,
         max_connections: parseInt(maxConnections),
-        open_connections: openConnections
-      }
-    }
-});
-  
+        open_connections: openConnections,
+      },
+    },
+  });
 }
 
 export default status;
