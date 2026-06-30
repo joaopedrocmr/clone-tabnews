d92c06030f2744baf29b6f64816c24be15987e9a
Joao Pedro <joaocardossomoreira@gmail.com>
2026-06-29 21:22:15 -0300

    makes npm test more robust with async-retry and orchestrator

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,18 +1,23 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
 test("GET to /api/v1/status should return 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
 
-  const resbody = await res.json()
- 
-  const parsedUpdatedAt = new Date(resbody.updated_at).toISOString()
-  expect(resbody.updated_at).toEqual(parsedUpdatedAt)
+  const resbody = await res.json();
 
-  expect(resbody.dependencies.database.version).toBeDefined()
-  expect(resbody.dependencies.database.version).toEqual("16.0")
+  const parsedUpdatedAt = new Date(resbody.updated_at).toISOString();
+  expect(resbody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(resbody.dependencies.database.max_connections).toBeDefined()
-  expect(resbody.dependencies.database.max_connections).toEqual(100)
+  expect(resbody.dependencies.database.version).toBeDefined();
+  expect(resbody.dependencies.database.version).toEqual("16.0");
 
-  expect(resbody.dependencies.database.open_connections).toEqual(1)
+  expect(resbody.dependencies.database.max_connections).toBeDefined();
+  expect(resbody.dependencies.database.max_connections).toEqual(100);
 
+  expect(resbody.dependencies.database.open_connections).toEqual(1);
 });
---
+++ tests/orchestrator.js
@@ -0,0 +1,25 @@
+import retry from "async-retry";
+
+async function waitForAllServices() {
+  await waitForWebServer();
+
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchStatusPage() {
+      const res = await fetch("http://localhost:3000/api/v1/status");
+      if (res.status !== 200) {
+        throw new Error(
+          `Web server is not ready yet. Status code: ${res.status}`,
+        );
+      }
+    }
+  }
+}
+
+export default {
+  waitForAllServices,
+};
