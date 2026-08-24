444a420a12f386d0957033ded3af69703209af39
Joao Pedro <joaocardossomoreira@gmail.com>
2026-08-24 20:08:02 -0300

    refactor: make api/v1/migrations use migrator model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,8 +1,6 @@
 import { createRouter } from "next-connect";
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database.js";
 import controller from "infra/controller.js";
+import migrator from "models/migrator.js";
 
 const router = createRouter();
 
@@ -11,67 +9,17 @@ router.post(postHandler);
 
 export default router.handler(controller.errorHandlers);
 
-const defaultMigrationOptions = {
-  dryRun: true,
-  dir: resolve("infra", "migrations"),
-  direction: "up",
-  verbose: true,
-  migrationsTable: "pgmigrations",
-};
-
-async function postHandler(req, res) {
-  let dbClient;
-
-  const allowedMethods = ["POST", "GET"];
-  if (!allowedMethods.includes(req.method)) {
-    return res.status(405).json({
-      error: `Method ${req.method} not allowed`,
-    });
-  }
-
-  try {
-    dbClient = await database.getNewClient();
-
-    const runMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dbClient: dbClient,
-      dryRun: false,
-    });
-
-    if (runMigrations.length > 0) {
-      return res.status(201).json(runMigrations);
-    }
-
-    return res.status(200).json(runMigrations);
-  } finally {
-    if (dbClient) {
-      await dbClient.end();
-    }
-  }
+async function getHandler(req, res) {
+  const pedingMigrations = await migrator.listPendingMigrations();
+  return res.status(200).json(pedingMigrations);
 }
 
-async function getHandler(req, res) {
-  const allowedMethods = ["POST", "GET"];
-  if (!allowedMethods.includes(req.method)) {
-    return res.status(405).json({
-      error: `Method ${req.method} not allowed`,
-    });
+async function postHandler(req, res) {
+  const runMigrations = await migrator.runPendingMigrations();
+
+  if (runMigrations.length > 0) {
+    return res.status(201).json(runMigrations);
   }
 
-  let dbClient;
-
-  try {
-    dbClient = await database.getNewClient();
-
-    const pedingMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dbClient: dbClient,
-    });
-
-    return res.status(200).json(pedingMigrations);
-  } finally {
-    if (dbClient) {
-      await dbClient.end();
-    }
-  }
+  return res.status(200).json(runMigrations);
 }
